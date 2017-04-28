bba1ab00a441cdb7c5ed62543acebbe5b4533aba
anhvurz90 <anhvurz90@gmail.com>
2017-04-28 14:22:49 +0700

    Add Level1-Bird&Coconut:1.PythonMath:1.1.Video-PythonMath

---
+++ codeschool/tutorial/tutorial.py
@@ -0,0 +1,45 @@
+Try Python: https://www.codeschool.com/courses/try-python
+
+Level 1: Bird & Coconuts: {
+	1.Python Math: {
+		1.1.Video-Python Math: {
+			- Why Python? {
+				+ Easy to learn
+				+ Easy to read
+				+ Fast
+				+ Versatile
+				+ Many libraries: Draw, Math, Game
+			}
+			- The Python Interpreter: {
+				>>> 35
+				35
+				>>> 30.5
+				30.5
+				>>> 30 + 5
+				35
+			}
+			- Mathematical Operators: {
+				addition:		>>> 3 + 5
+				subtraction:	>>> 10 - 5
+				multiplication:>>> 3 * 5
+				division:		>>> 30 / 6
+				exponent:	>>> 2 ** 3
+				negation:	>>> -2 + -3
+			}
+			- Integers and Floats: {
+				int: 35
+				float: 30.5
+			}
+			- Order of Operations: {
+				PEMDAS:
+					Parentheses, Exponent, Multiplication
+					Division, Addition, Subtraction
+			}
+			- Can a Swallow Carry a Coconut?: {
+				>>> 1450 / (60 / 3)
+			}
+		}
+	}
+}
+
+
