e7068242e7d6cce78c97f5338119ddfc75edd3ff
anhvurz90 <anhvurz90@gmail.com>
2017-05-04 21:51:33 +0700

    Add Tutorialspoint-Python3:09.Loops:09.01.LoopTypes:09.01.02.ForLoop-2ndPart

--- tutorialspoint/python3/script.py
+++ tutorialspoint/python3/script.py
@@ -1,18 +1,50 @@
+#WHILE LOOP--------------------------------------
+print('WHILE LOOP')
 count = 0
 while (count < 9):
 	print('The count is:', count)
 	count+= 1
 print('Good bye!')
-
+#--------------------------------------
 #var = 1
 #while var == 1:
 #	num = int(input("Enter a number: "))
 #	print("You entered: ", num)
 #print('Good bye!')
-
+#--------------------------------------
 count = 0
 while count< 5:
 	print (count, "is less than 5")
 	count+= 1
 else:
 	print (count, "is not less than 5")
+
+#FOR LOOP--------------------------------------
+print()
+print('FOR LOOP:')
+for var in list(range(5)):
+	print(var)
+#--------------------------------------
+for letter in 'Python':
+	print('Current Letter :', letter)
+#--------------------------------------
+fruits = ['banana', 'apple', 'mango']
+for fruit in fruits:
+	print('Current fruit :', fruit)
+#--------------------------------------
+for index in range(len(fruits)):
+	print('Current fruit with index: ', fruits[index])
+#--------------------------------------
+numbers = [1, 3, 5, 2, 7, 9, 11]
+for num in numbers:
+	if num %2 == 0:
+		print('The list contains an even number')
+		break
+else:
+	print('The list does not contain even number')
+#--------------------------------------
+#--------------------------------------
+#--------------------------------------
+#--------------------------------------
+#--------------------------------------
+#--------------------------------------
